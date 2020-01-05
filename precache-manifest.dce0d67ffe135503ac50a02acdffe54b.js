d0ef31bcd88fdaaec45d0a25653f4a4f73e82fd7
kmart691 <kmart691@naver.com>
2020-01-05 18:13:07 +0900

    Updates

---
+++ precache-manifest.dce0d67ffe135503ac50a02acdffe54b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664605d205d5982005db81bc895ed866",
+    "url": "/React-in-github-todo-list/index.html"
+  },
+  {
+    "revision": "7a603cfb45519e10a436",
+    "url": "/React-in-github-todo-list/static/css/main.dc65348f.chunk.css"
+  },
+  {
+    "revision": "4e36c210191f8cf4ce15",
+    "url": "/React-in-github-todo-list/static/js/2.a30c4477.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-in-github-todo-list/static/js/2.a30c4477.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a603cfb45519e10a436",
+    "url": "/React-in-github-todo-list/static/js/main.91bf73fb.chunk.js"
+  },
+  {
+    "revision": "ef123c4683a9e7884541",
+    "url": "/React-in-github-todo-list/static/js/runtime-main.a5ba32d7.js"
+  }
+]);
